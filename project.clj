12d43d0177ffe2001a27e83159324b422e033986
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2013-01-27 18:06:19 -0500

    Bump version to 1.0.0.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject cc.artifice/granary "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject cc.artifice/granary "1.0.0"
+  :description "Backend support such as database connectivity for Sisyphus, Retrospect, Chivalry, and other projects that use the same data."
+  :url "http://artifice.cc/granary"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/java.jdbc "0.2.3"]
-                 [korma "0.3.0-beta15-SNAPSHOT"]
+                 [korma "0.3.0-RC2"]
                  [mysql/mysql-connector-java "5.1.6"]])
