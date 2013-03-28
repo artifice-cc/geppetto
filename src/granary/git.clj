d3bc1d8a8e94aa0ec7ccc1e726e63762cbe40315
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2013-03-27 22:11:39 -0400

    Fixed some usage of old contrib libraries.

--- src/granary/git.clj
+++ src/granary/git.clj
@@ -1,20 +1,20 @@
 (ns granary.git
   (:use [clojure.string :only [split-lines trim]])
-  (:use [clojure.contrib.shell :only [sh]]))
+  (:use [clojure.java.shell :only [sh]]))
 
 (defn get-commit-date
   [git pwd commit]
-  (let [git-output (sh git (format "--work-tree=%s" pwd)
-                       "show" "--format=raw" commit)
+  (let [git-output (:out (sh git (format "--work-tree=%s" pwd)
+                             "show" "--format=raw" commit))
         timestamp (second (re-find #"committer .* (\d+) [-+]\d{4}" git-output))]
-    (sh "date" "+%Y-%m-%d %H:%M:%S" (format "--date=@%s" timestamp))))
+    (:out (sh "date" "+%Y-%m-%d %H:%M:%S" (format "--date=@%s" timestamp)))))
 
 (defn git-meta-info
   [git pwd]
-  (let [[out _ _ _ & msg] (split-lines (sh git (format "--work-tree=%s" pwd)
-                                           "log" "-n" "1"))
-        branch (trim (subs (sh git (format "--work-tree=%s" pwd)
-                               "branch" "--contains") 2))
+  (let [[out _ _ _ & msg] (split-lines (:out (sh git (format "--work-tree=%s" pwd)
+                                                 "log" "-n" "1")))
+        branch (trim (subs (:out (sh git (format "--work-tree=%s" pwd)
+                                     "branch" "--contains")) 2))
         commit (subs out 7)]
     {:commit commit
      :commitdate (get-commit-date git pwd commit)
