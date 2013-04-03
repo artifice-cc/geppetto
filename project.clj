06d4443c1d3da83c1d9db272adcb4914d4d64820
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2013-04-03 00:52:34 -0400

    Attempting fix with dependencies.
    Running retrospect verify-claims:
    
    java.lang.ClassNotFoundException: org.apache.commons.math3.distribution.NormalDistribution

--- project.clj
+++ project.clj
@@ -3,14 +3,13 @@
   :url "http://artifice.cc/geppetto"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :profiles {:user {:dependencies
-                    [[org.clojure/clojure "1.4.0"]
-                     [org.clojure/java.jdbc "0.2.3"]
-                     [korma "0.3.0-RC2"]
-                     [mysql/mysql-connector-java "5.1.6"]
-                     [org.apache.commons/commons-math3 "3.0"]
-                     [clojure-csv/clojure-csv "2.0.0-alpha1"]
-                     [incanter "1.5.0-SNAPSHOT"]
-                     [resque-clojure "0.2.2"]]}
-             :dev {:dependencies
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/java.jdbc "0.2.3"]
+                 [korma "0.3.0-RC2"]
+                 [mysql/mysql-connector-java "5.1.6"]
+                 [org.apache.commons/commons-math3 "3.0"]
+                 [clojure-csv/clojure-csv "2.0.0-alpha1"]
+                 [incanter "1.5.0-SNAPSHOT"]
+                 [resque-clojure "0.2.2"]]
+  :profiles {:dev {:dependencies
                    [[com.h2database/h2 "1.3.171"]]}})
