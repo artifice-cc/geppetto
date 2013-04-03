129abcdb6c2065ab20415581b2ab88915f2f0edc
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2013-04-03 00:53:18 -0400

    Move to SNAPSHOT because incanter is in SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cc.artifice/geppetto "2.2.0"
+(defproject cc.artifice/geppetto "2.3.0-SNAPSHOT"
   :description "Backend support for experimental work."
   :url "http://artifice.cc/geppetto"
   :license {:name "Eclipse Public License"
