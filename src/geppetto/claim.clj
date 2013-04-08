3afccff2a489391f3bc300235f62e8cc9d4a8f96
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2013-04-08 15:05:38 -0400

    Possibly fixed claim evaluation by using a ref for the results.

--- src/geppetto/claim.clj
+++ src/geppetto/claim.clj
@@ -1,21 +1,22 @@
 (ns geppetto.claim
   (:require [clojure.string :as str])
+  (:use [clojure.pprint :only [pprint]])
   (:use [clojure.walk])
   (:use [geppetto.parameters :only [read-params]])
   (:use [geppetto.records :only [run-with-new-record read-archived-results]])
   (:use [geppetto.stats])
   (:use [geppetto.random]))
 
+(def results (ref []))
+
 (defn replace-keys
-  [results resultstype form]
+  [resultstype form]
   (if (and (keyword? form) (re-matches #"^_.*" (name form)))
     (let [k (keyword (str/replace (name form) #"^_" ""))]
       `(map (fn [r#] (get r# ~k))
           (map (fn [rs-alltypes#]
                (last (get rs-alltypes# ~resultstype)))
-             ;; put it in a vec so it is not evaled as ({:control ...})
-             ;; but rather as [{:control ...}]
-             [~@results])))
+             @results)))
     form))
 
 (defmacro make-claim
@@ -34,36 +35,35 @@
                    ~(zipmap
                      [:control :comparison :comparative]
                      (for [resultstype [:control :comparison :comparative]]
-                       (vec (for [v (get (first params) resultstype)]
-                              `{:code '~v
-                                :result (fn [results#]
-                                          (eval (postwalk
-                                                 (fn [term#]
-                                                   (replace-keys results# ~resultstype term#))
-                                                 '~v)))}))))}))))
+                       (vec
+                        (for [v (get (first params) resultstype)]
+                          (let [result-fn (postwalk #(replace-keys resultstype %) v)]
+                            `{:code '~v
+                              :result (fn [] ~result-fn)})))))}))))
 
 (defn evaluate-claim
   [run-fn claim datadir git recordsdir nthreads]
   (let [seed 1
         repetitions 30
-        results (binding [rgen (new-seed seed)]
-                  (run-with-new-record
-                    run-fn (:parameters claim) datadir seed git recordsdir
-                    nthreads repetitions false true true))
-        verifications (apply concat
-                             (for [resultstype [:control :comparison :comparative]]
-                               (for [to-verify (get (:verify claim) resultstype)]
-                                 {:code (:code to-verify)
-                                  :resultstype resultstype
-                                  :verification-result ((:result to-verify) results)})))]
-    (println verifications)
-    (doseq [ver verifications]
-      (println (format "%s (%s):\t %s" (if (:verification-result ver) "PASS" "FAIL")
-                  (name (:resultstype ver)) (:code ver))))
-    (if (every? :verification-result verifications)
-      (do
-        (println (format "Claim \"%s\" verified." (:name claim)))
-        true)
-      (do
-        (println (format "Claim \"%s\" not verified." (:name claim)))
-        false))))
+        run-results (binding [rgen (new-seed seed)]
+                      (run-with-new-record
+                        run-fn (:parameters claim) datadir seed git recordsdir
+                        nthreads repetitions false true true))]
+    (dosync (alter results (constantly run-results)))
+    (let [verifications (apply concat
+                               (for [resultstype [:control :comparison :comparative]]
+                                 (for [to-verify (get (:verify claim) resultstype)]
+                                   {:code (:code to-verify)
+                                    :resultstype resultstype
+                                    :verification-result ((:result to-verify))})))]
+      (doseq [ver verifications]
+        (println (format "%s (%s):" (if (:verification-result ver) "PASS" "FAIL")
+                    (name (:resultstype ver))))
+        (pprint (:code ver)))
+      (if (every? :verification-result verifications)
+        (do
+          (println (format "Claim \"%s\" verified." (:name claim)))
+          true)
+        (do
+          (println (format "Claim \"%s\" not verified." (:name claim)))
+          false)))))
--- test/geppetto/claim_test.clj
+++ test/geppetto/claim_test.clj
@@ -11,28 +11,34 @@
 (deftest test-make-claim
   (let [claim (make-claim tracking-baseline-high-avgprec
                           (parameters "Tracking/baseline")
-                          (verify {:control ((> (geppetto.stats/mean :_AvgPrec) 0.75)
-                                             (> (geppetto.stats/mean :_AvgCoverage) 0.75))}))]
+                          (verify {:control
+                                   ((> (geppetto.stats/mean :_AvgPrec) 0.75)
+                                    (> (geppetto.stats/mean :_AvgCoverage) 0.75))}))]
     (is (= 'tracking-baseline-high-avgprec (:name claim)))
     (is (= "Tracking/baseline" (:parameters claim)))
-    (is (= '(> (geppetto.stats/mean :_AvgPrec) 0.75) (:code (first (:control (:verify claim))))))
-    (is (= '(> (geppetto.stats/mean :_AvgCoverage) 0.75) (:code (second (:control (:verify claim))))))
-    (is (= false ((:result (first (:control (:verify claim))))
-                  [{:control [{:AvgPrec 1.0}]}
-                   {:control [{:AvgPrec 0.0}]}])))
-    (is (= true ((:result (first (:control (:verify claim))))
-                 [{:control [{:AvgPrec 1.0}]}
-                  {:control [{:AvgPrec 2.0}]}])))))
+    (is (= '(> (geppetto.stats/mean :_AvgPrec) 0.75)
+           (:code (first (:control (:verify claim))))))
+    (is (= '(> (geppetto.stats/mean :_AvgCoverage) 0.75)
+           (:code (second (:control (:verify claim))))))
+    (is (= false (do (dosync (alter results
+                                    (constantly [{:control [{:AvgPrec 1.0}]}
+                                                 {:control [{:AvgPrec 0.0}]}])))
+                     ((:result (first (:control (:verify claim))))))))
+    (is (= true (do (dosync (alter results
+                                   (constantly [{:control [{:AvgPrec 1.0}]}
+                                                {:control [{:AvgPrec 2.0}]}])))
+                    ((:result (first (:control (:verify claim))))))))))
 
 (deftest test-evaluate-claim
-  (let [claim (make-claim tracking-baseline-high-avgprec
-                          (parameters "Testing/test-1")
-                          (verify {:control
-                                   ((< (geppetto.stats/mean :_a) 1.0)
-                                    (> (geppetto.stats/mean :_b) 0.0)
-                                    (let [lm (geppetto.stats/linear-reg :_a :_b)]
-                                      (and (geppetto.test-utils/nearly= (first (:coefs lm)) 10.0 1.0)
-                                           (> (:r-square lm) 0.8))))}))
+  (let [claim (make-claim
+               tracking-baseline-high-avgprec
+               (parameters "Testing/test-1")
+               (verify {:control
+                        ((< (geppetto.stats/mean :_a) 1.0)
+                         (> (geppetto.stats/mean :_b) 0.0)
+                         (let [lm (geppetto.stats/linear-reg :_a :_b)]
+                           (and (geppetto.test-utils/nearly= (first (:coefs lm)) 10.0 1.0)
+                                (> (:r-square lm) 0.8))))}))
         run-fn (fn [comparative? params] (let [a (rand) b (* (+ 10 (rand)) a)] [{:a a :b b}]))
         eval-result (evaluate-claim run-fn claim "" "/usr/bin/git" "/tmp" 1)]
     (is (= true eval-result))))
